49cffbdcc2b99507b63356456c288c94bf5bfa51
Prathmesh Wakodikar <46840479+Prathm98@users.noreply.github.com>
2021-01-09 18:16:33 +0530

    Spinner, axios
    Added spinner component and calling github api. Added local env variables.

--- github-finder/src/App.js
+++ github-finder/src/App.js
@@ -2,14 +2,27 @@ import React, { Component } from 'react';
 import './App.css';
 import Navbar from './components/layout/Navbar';
 import Users from './components/users/Users';
+import axios from 'axios';
 
 class App extends Component {  
+
+  state = {
+    loading: false,
+    users: [] 
+  };
+
+  async componentDidMount() {    
+    this.setState({loading: true});
+    const res = await axios.get('https://api.github.com/users?client_id='+ process.env.REACT_APP_GIT_ID + 'client_secrete=' + process.env.REACT_APP_GIT_SECRETE);
+    this.setState({loading: false, users: res.data});    
+  }
+
   render() {
     return (
       <div className="App">
         <Navbar title='Github Finder' icon="fab fa-github" />
         <div className='container'>
-          <Users />
+          <Users loading={this.state.loading} users={this.state.users} />
         </div>        
       </div>
     );
